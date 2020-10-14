8e46bc47e6c0baf9b7c98038d7fc043af244a5e1
Tom Freeman <fresh.lime.lemon@gmail.com>
2020-10-14 14:12:20 +0300

    Added missed lecftions and done a homework of lesson3

---
+++ Lesson_03 (missed classes)/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Lesson_03__missed_classes_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task01();
+        }
+
+        private static void Task01()
+        {
+            // Initialization
+            string[] names = { "Ivan", "Peter", "John", "Jane", "Maria" };
+
+            DateTime[] bdays = new DateTime[names.Length];
+            bdays[0] = new DateTime(2000, 01, 22);
+            bdays[1] = new DateTime(2001, 01, 02);
+            bdays[2] = new DateTime(2002, 01, 02);
+            bdays[3] = new DateTime(2003, 01, 02);
+            bdays[4] = new DateTime(2004, 01, 02);
+
+            // User input
+            Console.Write("Please enter index (0-4): ");
+            int index = int.Parse(Console.ReadLine());
+
+            // Program logic
+            string periodOfMonth = GetPeriodOfMonth(bdays, index);
+            string monthName = GetNameOfMonth(bdays[index].Month);
+
+            // Output
+            Console.WriteLine($"{names[index]}'s is born in the {periodOfMonth} in {monthName} ");
+        }
+
+        private static string GetPeriodOfMonth(DateTime[] bdays, int index)
+        {
+            int day = bdays[index].Day;
+            string periodOfMonth = "beginning";
+
+            if (day > 10)
+            {
+                periodOfMonth = "middle";
+            }
+
+            if (day > 20)
+            {
+                periodOfMonth = "end";
+            }
+
+            return periodOfMonth;
+        }
+
+        private static string GetNameOfMonth(int month)
+        {
+            if (month == 1) return "January";
+            if (month == 2) return "February";
+            if (month == 3) return "March";
+            if (month == 4) return "April";
+            if (month == 5) return "May";
+            if (month == 6) return "Juny";
+            if (month == 7) return "July";
+            if (month == 8) return "August";
+            if (month == 9) return "September";
+            if (month == 10) return "October";
+            if (month == 11) return "November";
+            if (month == 12) return "December";
+
+            return "Unknown";
+        }
+    }
+}
+
+
---
+++ Lesson_03 Homework/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson_03_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise1();
+        }
+
+        private static void Task4List()
+        {
+            var names = new List<string>();
+            names.Add("Aaron");
+            names.Add("Archie");
+            names.Add("Third");
+            names.Add("Hi there");
+            names.Add("Ashley");
+            names.Add("Bob");
+            names.Add("Dilan");
+            names.Add("Daniel");
+            names.Add("Elon");
+            names.Add("Edgar");
+
+            if (names.Contains("Ashley"))
+            {
+                Console.WriteLine("There's an Item with name Ashley in List");
+            }
+            foreach (var listItem in names)
+            {
+                Console.WriteLine(listItem);
+            }
+
+
+            Console.ReadKey();
+        }
+
+        private static void Task3Queue()
+        {
+            Queue names = new Queue();
+            names.Enqueue(1254);
+            names.Enqueue(3);
+            names.Enqueue("Third");
+            names.Enqueue("Hi there");
+            names.Enqueue("Ashley");
+            names.Enqueue("Bob");
+            names.Enqueue("Dilan");
+            names.Enqueue("Daniel");
+            names.Enqueue("Elon");
+            names.Enqueue("Edgar");
+
+            if (names.Contains(3))
+            {
+                Console.WriteLine("There's an Item with name 3 in Stack");
+            }
+            foreach(var queueItem in names)
+            {
+                Console.WriteLine(queueItem);
+            }
+
+
+            Console.ReadKey();
+        }
+
+        private static void Task2Stack()
+        {
+            Stack names = new Stack();
+            names.Push("Ashley");
+            names.Push("Bob");
+            names.Push("Dilan");
+            names.Push("Daniel");
+            names.Push("Elon");
+            names.Push("Edgar");
+            names.Push("Finn");
+            names.Push("George");
+            names.Push("Hilary");
+            names.Push("Issac");
+
+            if(names.Contains ("Bob"))
+            {
+                Console.WriteLine("There's an Item with name Bob in Stack");
+            }
+
+            foreach (object stackItem in names)
+            {
+                Console.WriteLine(stackItem);
+            }
+
+            Console.ReadKey();
+
+
+        }
+
+        private static void Task1Array()
+        {
+
+            string[] names = new string[10];
+            names[0] = "Ashley";
+            names[1] = "Bob";
+            names[2] = "Dilan";
+            names[3] = "Daniel";
+            names[4] = "Elon";
+            names[5] = "Edgar";
+            names[6] = "Finn";
+            names[7] = "George";
+            names[8] = "Hilary";
+            names[9] = "Issac";
+
+            if (names.Contains("Elon"))
+            {
+                Console.WriteLine("There's a name Elon, 5th in array");
+            }
+
+            Array.ForEach(names, Console.WriteLine);
+
+            Console.ReadKey();
+        }
+
+        private static void Exercise1()
+        {
+
+
+        }
+
+
+
+    }
+}
