bcbf71e03756b2fafd3278355e1d52c1c53bb97c
Tom Freeman <fresh.lime.lemon@gmail.com>
2020-10-27 20:13:19 +0200

    9

---
+++ Lesson_04/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+
+namespace Lesson_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1Menu();
+        }
+        
+
+
+
+
+
+
+
+        private static void Task1Menu()
+        {
+            while(true)
+            {
+                List<int> listItems = new List<int>();
+
+                Console.Clear();
+                Console.WriteLine("1. Add Item");
+                Console.WriteLine("2. Remove Item");
+                Console.WriteLine("3. List of all Items");
+                Console.WriteLine("4. Exit");
+
+                string choise = Console.ReadLine();
+                
+                switch (choise)
+                {
+                    case "1":
+                        Console.WriteLine(choise);
+                            break;
+                    case "2":
+                        Console.WriteLine(choise);
+                        break;
+                    case "3":
+                        Console.WriteLine(choise);
+                        break;
+                    case "4":
+                        Console.WriteLine(choise);
+                        return;
+                }
+
+            }
+            static void AddItem()
+            {
+                Console.Write("Add Item = ");
+                int item = int.Parse(Console.ReadLine());
+                listItems.Add(item);
+            }
+            static void RemoveItem()
+            {
+                Console.Write("Remove Item = ");
+                int item = int.Parse(Console.ReadLine());
+                listItems.Remove(item);
+            }
+
+        }
+        
+
+        private static void StudentMarks()
+        {
+            Dictionary<string, int> studentMarks = new Dictionary<string, int>();
+
+            studentMarks.Add("Jane", 90);
+            studentMarks.Add("John", 80);
+            studentMarks.Add("Ivan", 70);
+            studentMarks.Add("Peter", 60);
+            studentMarks.Add("Maria", 50);
+
+            //putting in loop:
+            while (true)
+            {
+                //displaying all marks and students:
+                foreach (var studentMark in studentMarks)
+                {
+                    Console.Write($"Student {studentMark.Key}, mark={studentMark.Value}");
+                }
+
+
+                string name = Console.ReadLine();
+                Console.Write("Name: ");
+                //exiting programm if needed:
+                if (name == "exit")
+                {
+                    break;
+                }
+                
+
+                if (studentMarks.ContainsKey(name))
+                {
+                    Console.WriteLine($"Student '{name}' score is {studentMarks[name]}");
+                }
+                else
+                {
+                    Console.WriteLine($"Student '{name}' does not exsist");
+                }
+            }
+
+            
+        }
+        private static void StudentMarks1()
+        {
+            string text = "a text some text just some text";
+            IDictionary<string, int> wordsCount = new SortedDictionary<string, int>();
+            string[] words = text.Split(' ');
+            foreach (string word in words)
+            {
+                int count = 1;
+                if (wordsCount.ContainsKey(word))
+                {
+                    count = wordsCount[word] + 1;
+                }
+                wordsCount[word] = count;
+            }
+            
+
+
+
+        }
+
+    }
+}
---
+++ Lesson_05/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        private static void MinMaxArray()
+        {
+            //task: find min and max elements in the array
+            //~~~~~~~~~~~~~~~~~~~~~~~~~
+            //1. Start
+            //2. Define min, max
+            
+            //3. Read array
+            
+            //4. min=array[0], max=array[0]
+            //5. foreach element of array
+            //      5.1 If element < min 
+            //          min=element
+            //      5.2 if element > max
+            //          max=element
+            //6. Display min, max
+            //7. End
+
+
+
+
+        }
+
+        private static void Average()
+        {
+            // Task: calculate average
+            // ~~~~~~~~~~~~~~~~~~~
+            //Start
+            //2. Define sum, count, avg
+            //3. Read array
+            //4. foreach element of the array 
+            //    4.1 sum = sum + element
+            //    4.2 count++
+            //5. avg = sum / count
+            //6. Display avg
+            // 7. End
+        }
+        private static void Swap()
+        {
+            // Task: swap 2 array items
+            // {1, 2, 3, 4} => swap 1 and 3 => {3, 2, 1, 4}
+            // ~~~~~~~~~~~~~~~~~~~
+            // 1. Start
+            // 2. Define temp
+            // 3. Read array
+            // 4. Read index1 (0) and index2 (2)
+            // 5. temp = array[index1]
+            // 6. Array[Index1] = array [index2]
+            // 7. array[Index2] = temp
+            // 8. End 
+        }
+        private static void PushStack()
+        {
+            //Add element to stack realized by array
+            //~~~~~~~~~~~~~~~~~~~~~~~~
+            //1. Start
+            //2. Define element, is added
+            //3. Read new element
+            //4. isadded = full
+            //5. For each item in array 
+            //    If item is empty
+            //     5.1 Item = element
+            //      5.2 isadded = true
+            //6. Return isadded
+            //7. End
+        }
+        private static void PopStack()
+        {
+            // Remove item from stack
+            // {1, 2, 3, null, null, null} ==> 3
+            //~~~~~~~~~~~~~~~~~~~~~~
+            //1. Start
+            //2. Define output
+            //3. Foreach item in array
+            //   If item is null
+            //       3.1 output = array[item index -1]
+            //       3.2 array[item index -1] = null
+            //       3.3 Exit foreach loop
+            //4. Return output
+            //5. End
+
+        }
+        private static void AddTreeElem()
+        {
+            //Add Element into binary tree
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+        }
+
+    }
+}
